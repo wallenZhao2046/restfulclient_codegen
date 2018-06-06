9189d2b7e4f656453a32a85e3074c6f1dbb61cbc
zhaowanlun <zhaowanlun@huobi.com>
2018-06-06 10:55:34 +0800

    make comment more clear

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
-## input swagger format and output behave code with tag
-python code_generator.py -i test_data/swagger-ui-sample.json -f swagger -o tmp/001.txt  -t behave -TAG mgt-api-controller
-## input swagger format and output unittest code
-python code_generator.py -i test_data/swagger-ui-sample.json -o tmp/001.txt 
-## input har format and output unittest code
-python code_generator.py -i test_data/har_single.json -o tmp/002.txt
-## input har format and output behave code
+# ---------------input har format --------------------------
+## output behave code
 python code_generator.py -i test_data/har_double.json -o tmp/002.txt -t behave
-## input postman collection export file and output behave code
+
+## output unittest code
+python code_generator.py -i test_data/har_single.json -o tmp/002.txt
+
+# -------------- input swagger ui format ---------------------
+## output behave code with tag
+python code_generator.py -i test_data/swagger-ui-sample.json -f swagger -o tmp/001.txt  -t behave -TAG mgt-api-controller
+
+## output unittest code
+python code_generator.py -i test_data/swagger-ui-sample.json -o tmp/001.txt 
+
+# ----------------input postman collection export format -------------------------
+## output behave code
 python code_generator.py -i test_data/postman_collection.json -f postman -o tmp/003.txt -t behave
