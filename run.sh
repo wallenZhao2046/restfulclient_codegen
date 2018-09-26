d4c898acfe4c67412dadbc34d1110f206f7d1e0e
zhaowanlun <zhaowanlun@huobi.com>
2018-09-26 14:10:43 +0800

    api service code generation sample

--- run.sh
+++ run.sh
@@ -15,3 +15,6 @@ python code_generator.py -i test_data/swagger-ui-sample.json -o tmp/001.txt
 # ----------------input postman collection export format -------------------------
 ## output behave code
 python code_generator.py -i test_data/postman_collection.json -f postman -o tmp/003.txt -t behave
+
+## ouput api service code
+python code_generator.py -i test_data/swagger-ui_0920.json -t service -f swagger -TAG dw-mgt-controller   -o tmp/dw-mgt-controller.py
